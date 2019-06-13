cdbd0225b7218cd8574cd64c4636271f2e227e6d
Bartłomiej <b.popielarz@positiv.pl>
2019-06-13 13:34:03 +0200

    random solution example

--- InstanceSolvers/Moves/Insert.cs
+++ InstanceSolvers/Moves/Insert.cs
@@ -19,52 +19,96 @@ namespace InstanceSolvers.Moves
         public AdvertisementOrder AdvertisementOrder { get; set; }
         public int Position { get; set; }
 
-        private Dictionary<int, TaskData> TaskDataBeforeMove { get; set; }
-        private Dictionary<int, TaskData> TaskDataAfterMove { get; set; }
+        private Dictionary<int, TaskData> _changedOrderStatsBefore { get; set; }
+        private Dictionary<int, TaskData> _changedOrderStatsAfter { get; set; }
+        private Dictionary<int, TaskData> _currentBreakAssesment { get; set; }
+        private Dictionary<int, TaskData> _afterMoveAssesment { get; set; }
         public Dictionary<int, TaskCompletionDifference> CompletionDifferences { get; set; }
 
-        public void Asses()
+        private void RollBackSolutionScores()
         {
-            var currentSchedule = Solution.AdvertisementsScheduledOnBreaks[TvBreak.ID];
-            var currentBreakAssesment = Solution.GradingFunction.AssesBreak(currentSchedule);
-            var orderPostMove = currentSchedule.Order.ToList();
-            orderPostMove.Insert(Position, AdvertisementOrder);
-            var assessmentAfterMove = Solution.GradingFunction.AssesBreak(new BreakSchedule(TvBreak) { BreakData = TvBreak, Order = orderPostMove });
-            TaskDataBeforeMove = Solution.AdOrderData;
-            TaskDataAfterMove = TaskDataBeforeMove.ToDictionary(a => a.Key, a => a.Value.Clone());
-            foreach(var taskData in TaskDataAfterMove)
+            foreach (var taskData in _afterMoveAssesment.Values)
             {
-                if(currentBreakAssesment.TryGetValue(taskData.Key, out var oldBreakData))
-                {
-                    taskData.Value.RemoveOtherDataFromThis(oldBreakData);
-                }
-                if(assessmentAfterMove.TryGetValue(taskData.Key, out var newBreakData))
-                {
-                    taskData.Value.MergeOtherDataIntoThis(newBreakData);
-                }
+                Solution.AdOrderData[taskData.TaskID].RemoveOtherDataFromThis(taskData);
             }
-            CompletionDifferences = TaskDataAfterMove.ToDictionary(d => d.Key, d => d.Value.CalculateDifference(TaskDataBeforeMove[d.Key]));
+            foreach (var taskData in _currentBreakAssesment.Values)
+            {
+                Solution.AdOrderData[taskData.TaskID].MergeOtherDataIntoThis(taskData);
+            }
+        }
+
+        private void AddToSolutionScores()
+        {
+            _changedOrderStatsBefore = new Dictionary<int, TaskData>();
+            _changedOrderStatsAfter = new Dictionary<int, TaskData>();
+            CompletionDifferences = new Dictionary<int, TaskCompletionDifference>();
+            foreach (var taskData in _currentBreakAssesment.Values)
+            {
+                TaskData statsCopy = new TaskData() { AdvertisementOrderData = taskData.AdvertisementOrderData };
+                statsCopy.OverwriteStatsWith(taskData);
+                _changedOrderStatsBefore.Add(statsCopy.TaskID, statsCopy);
+                Solution.AdOrderData[taskData.TaskID].RemoveOtherDataFromThis(taskData);
+            }
+            foreach (var taskData in _afterMoveAssesment.Values)
+            {
+                if(!_changedOrderStatsBefore.TryGetValue(taskData.TaskID, out TaskData statsCopy))
+                {
+                    statsCopy = new TaskData() { AdvertisementOrderData = taskData.AdvertisementOrderData };
+                    statsCopy.OverwriteStatsWith(taskData);
+                    _changedOrderStatsBefore.Add(statsCopy.TaskID, statsCopy);
+                }
+                Solution.AdOrderData[taskData.TaskID].MergeOtherDataIntoThis(taskData);
+            }
+            foreach(var statsBefore in _changedOrderStatsBefore.Values)
+            {
+                TaskData statsCopy = new TaskData() { AdvertisementOrderData = statsBefore.AdvertisementOrderData };
+                statsCopy.OverwriteStatsWith(Solution.AdOrderData[statsBefore.TaskID]);
+                _changedOrderStatsAfter.Add(statsBefore.TaskID, statsCopy);
+                CompletionDifferences.Add(statsBefore.TaskID, statsCopy.CalculateDifference(statsBefore));
+            }
             OverallDifference = new TaskCompletionDifference();
-            foreach(var difference in CompletionDifferences)
+            foreach (var difference in CompletionDifferences)
             {
                 OverallDifference.Add(difference.Value);
             }
         }
 
+        private void CountBreakTaskChanges()
+        {
+            var currentSchedule = Solution.AdvertisementsScheduledOnBreaks[TvBreak.ID];
+            _currentBreakAssesment = Solution.GradingFunction.AssesBreak(currentSchedule);
+            var orderPostMove = currentSchedule.Order.ToList();
+            orderPostMove.Insert(Position, AdvertisementOrder);
+            _afterMoveAssesment = Solution.GradingFunction.AssesBreak(new BreakSchedule(TvBreak) { BreakData = TvBreak, Order = orderPostMove });
+        }
+
+        public void Asses()
+        {
+            CountBreakTaskChanges();
+            AddToSolutionScores();
+            RollBackSolutionScores();
+        }
+
         public void Execute()
         {
-            if(TaskDataAfterMove == null)
+            if (OverallDifference == null)
             {
                 Asses();
             }
             Solution.AddAdToBreak(AdvertisementOrder, TvBreak, Position);
-            Solution.AdOrderData = TaskDataAfterMove;
+            foreach (var statsAfter in _changedOrderStatsAfter.Values)
+            {
+                Solution.AdOrderData[statsAfter.TaskID].OverwriteStatsWith(statsAfter);
+            }
         }
 
         public void RollBack()
         {
             Solution.RemoveAdFromBreak(AdvertisementOrder, TvBreak, Position);
-            Solution.AdOrderData = TaskDataBeforeMove;
+            foreach (var statsBefore in _changedOrderStatsBefore.Values)
+            {
+                Solution.AdOrderData[statsBefore.TaskID].OverwriteStatsWith(statsBefore);
+            }
         }
     }
 }
