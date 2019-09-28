0753ef4099b890807a2ef62e9b86679b41310f87
Mustrumion <bar.popiel@gmail.com>
2019-09-28 23:08:26 +0200

    Updated visualizations

--- Visualisation/evolutionaryColumnChartGrid.py
+++ Visualisation/evolutionaryColumnChartGrid.py
@@ -48,21 +48,15 @@ def get_channels_from_path(path: str):
     return int(char) 
 
 
-directory = 'C:/Users/Mustrum/Dropbox/MDP/solutions/annealing_best_tests_all2/'
+directory = 'C:/Users/Mustrum/Dropbox/MDP/solutions/evolutionary_best_tests_all2/'
 fileMask = '**/0*.json'
 files = [f for f in glob.glob(directory + fileMask, recursive=True)]
 
-columns=['name', 'difficulty', 'horizon', 'channel count', 'accept. function', 'accept. f. % improv.', 'acceptable %', 'loss [x1000]', 'loss % improvement', 'time [min]', 'iterations [x100 000]']
-df = pd.DataFrame(columns = columns)
-outliers = pd.DataFrame(columns = columns)
+columns=('difficulty', 'horizon', 'channel count', 'accept. function', 'accept. f. % improv.', 'acceptable %', 'loss [x1000]', 'loss % improvement', 'time [min]', 'generations')
+df = pd.DataFrame(index=np.arange(0, len(files)), columns = columns)
 
 for i, f in enumerate(files):
     with open(f) as json_file:
-        pathSegments = os.path.split(f)
-        fileName = os.path.splitext(pathSegments[1])[0]
-        parentDir = os.path.split(pathSegments[0])[1]
-        name = parentDir + "/" + fileName
-
         data = json.load(json_file)
         loss = data['WeightedLoss'] / 1000
         lossPercent = (data['WeightedLossBefore'] - data['WeightedLoss']) / data['WeightedLossBefore'] * 100
@@ -73,32 +67,24 @@ for i, f in enumerate(files):
             accetabilityPercent = (data['IntegrityLossScoreBefore'] - data['IntegrityLossScore']) / data['IntegrityLossScoreBefore'] * 100
         accetabilePercent = (data['IntegrityLossScore'] == 0) * 100.0
         time = get_minutes(data['LastTimeElapsed'])
-        iterations = data['NumberOfIterations'] / 100000
+        if time > 69.99:
+            print(f)
+        iterations = data['NumberOfIterations']
 
         difficulty = get_difficulty_from_path(f)
         horizon = get_length_from_path(f)
         channels = get_channels_from_path(f)
 
-        if time > 59.99:
-            print(f)
-            outliers.loc[i] = [name, difficulty, horizon, channels,
-                accetability, accetabilityPercent, accetabilePercent, loss, lossPercent, time, iterations]
-        else:
-            df.loc[i] = [name, difficulty, horizon, channels,
+        df.loc[i] = [difficulty, horizon, channels,
                 accetability, accetabilityPercent, accetabilePercent, loss, lossPercent, time, iterations]
 
-df[columns[1:4]] = df[columns[1:4]].astype(str) 
-df[columns[4:]] = df[columns[4:]].apply(pd.to_numeric)
 # df = df.apply(pd.to_numeric)
-df.sort_values(by=['horizon', 'name'], inplace=True, ascending=[False, True])
-outliers.sort_values(by=['horizon', 'name'], inplace=True, ascending=[False, True])
-df.to_csv (directory + 'plottedSolutionsData.csv', index = None, header=True)
-outliers[['loss [x1000]', 'loss % improvement', 'iterations [x100 000]']]= outliers[['loss [x1000]', 'loss % improvement', 'iterations [x100 000]']].round(3)
-outliers.to_csv (directory + 'outliersSolutionsData.csv', index = None, header=True)
+df.sort_values(by=['horizon'], inplace=True, ascending=False)
+df.to_csv (directory + 'gatheredSolutionsData.csv', index = None, header=True)
 print(df)
-g = sns.PairGrid(df, x_vars=columns[1:4], y_vars=columns[4:])
+g = sns.PairGrid(df, x_vars=columns[0:3], y_vars=columns[3:])
 g = g.map(sns.barplot, ci = 75)
 plt.subplots_adjust(top=0.95, bottom=0.05)
-g.fig.suptitle("Simulated annealing results categorized by the instances")
+g.fig.suptitle("Evolutionary algorithm results categorized by the instances")
 # plt.tight_layout()
 plt.show()
